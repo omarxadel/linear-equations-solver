ce5ec1e45a85128b68934d698bcc3a73cb40b3f7
Hoda Amr <48572265+hodaamr@users.noreply.github.com>
2021-06-12 02:09:29 +0200

    Add gauss seidel

---
+++ gauss_seidel.m
@@ -0,0 +1,37 @@
+function [output_args] = gauss_seidel(A, B)
+%Gauss_seidel Find the solution to a system of linear equations using
+%Gauss_seidel Elimination
+%   Input:
+%           A: Matrix of coeffecients 
+%           B: Matrix of constants
+%   This function takes matrix of coeffecients and the matrix of constants
+%   and computes the solution to the system of linear equations using
+%   Gaussian-Seidel Elimination
+
+
+tol = 0.001;
+    
+% Separation of matrix A into lower triangular and upper triangular matrices
+% A = D + L + U
+D = diag(diag(A));
+L = tril(A)- D;
+U = triu(A)- D;
+
+X0 = ones(na,1);
+
+% allowable error in final answer
+t = tol*ones(na,1);
+k= 1;
+
+X( : , 1 ) = X0;
+err= 1000000000*rand(na,1);                                     % initial error assumption for looping
+while sum(abs(err) >= t) ~= zeros(na,1)
+    X ( : ,k+ 1 ) = -inv(D+L)*(U)*X(:, k) + inv(D+L)*B;        % Gauss-Seidel formula
+    err = X( :,k+1) - X( :, k);                                 % finding error
+    k = k + 1;
+end
+
+output_args=X(:, k);
+
+end
+
